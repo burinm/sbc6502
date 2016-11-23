4a729166bc2f22ebad7b6ae0f4592f12019d5207
burin <Micheal.Burin@colorado.edu>
2016-11-23 18:08:18 +0000

    fm25640b sucessfully getting Status and setting WREN/WRDI

--- loader.c
+++ loader.c
@@ -7,13 +7,19 @@
 //  and testing a bit banged SPI interface
 
 int main() {
+uint8_t status0;
+uint8_t status1;
 
 
 fm25640b_open();
 
-fm25640b_get_status();
+fm25640b_set_write_enable();
+status0 = fm25640b_get_status();
+fm25640b_set_write_disable();
+status1 = fm25640b_get_status();
 
-    spi_readwrite_byte('U');
+printf ("0x%x 0x%x\n",status0,status1);
+
 
         
 
