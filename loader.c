1426e50d25f6477f86f473d37c8a0704d1c1ebfc
burin <Micheal.Burin@colorado.edu>
2016-11-23 18:45:42 +0000

    Sucessfully read/write FRAM with simple message

--- loader.c
+++ loader.c
@@ -1,5 +1,6 @@
 #include "spi.h"
 #include "util.h"
+#include <string.h>
 #include "fm25640b.h"
 
 //Beagle Bone or Linux only
@@ -9,17 +10,33 @@
 int main() {
 uint8_t status0;
 uint8_t status1;
+uint8_t i;
+uint8_t b;
 
+uint8_t message[]="Hello FRAM?";
+
+printf("message has %d bytes\n", strlen(message));
 
 fm25640b_open();
 
-fm25640b_set_write_enable();
 status0 = fm25640b_get_status();
+
+fm25640b_set_write_enable();
+fm25640b_write_block(0x000,(uint8_t*)message, strlen(message));
 fm25640b_set_write_disable();
+
 status1 = fm25640b_get_status();
 
 printf ("0x%x 0x%x\n",status0,status1);
 
+for (i=0;i<strlen(message);i++) {
+b = fm25640b_read_byte(i);
+printf("%c",(char)b);
+    
+
+
+}
+printf("\n");
 
         
 
