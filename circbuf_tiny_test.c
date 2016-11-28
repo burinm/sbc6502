79ea1731bc42b252c9972d00a855b5480992a7cc
burin <Micheal.Burin@Colorado.edu>
2016-11-28 15:32:56 -0700

    Adding tiny circular buffer library

---
+++ circbuf_tiny_test.c
@@ -0,0 +1,39 @@
+#include "circbuf_tiny.h"
+
+int main() {
+
+uint8_t b;
+uint16_t i;
+
+circbuf_tiny_t c;
+circbuf_tiny_init(&c);
+for (i=1;i<50;i++) {
+    circbuf_tiny_write(&c,i);
+    printf("write-->%d\n",i);
+}
+
+for (i=1;i<10;i++) {
+    circbuf_tiny_read(&c,&b);
+    printf("read-->%d\n",b);
+}
+
+for (i=88;i<99;i++) {
+    circbuf_tiny_write(&c,i);
+    printf("write-->%d\n",i);
+}
+
+for (i=0;i<51;i++) {
+    circbuf_tiny_read(&c,&b);
+    printf("read-->%d\n",b);
+}
+
+circbuf_tiny_destroy(&c);
+
+circbuf_tiny_init(&c);
+circbuf_tiny_destroy(&c);
+
+
+
+
+
+}
