4e2dac4b5ac12d2be20145bf8983a82f1c1aebe8
burin <Micheal.Burin@Colorado.edu>
2016-12-02 16:06:01 -0700

    Bootloader update with function pointer to 0x200 vector. SPI only initialize PortB lines it's going to use. Add debug port B2

--- bootloader.c
+++ bootloader.c
@@ -1,3 +1,45 @@
+/* sbc bootloader
+
+    This is the main routine that loads FRAM into
+     the main RAM at startup. The FRAM binary should
+     be in the following format:
+
+    byte0 [image size low] byte1 [image size high] 
+    byte2 [start addr low] byte1 [start addr high] 
+    byte4 [............]
+    byten+1 [checksum]
+
+    The [start] address
+     is 16 bit jump address to the _init call in the binary
+    The [checksum] is a running 8bit total of
+     bytes 0->n
+
+
+    The binary, from byte 2->n is loaded into memory
+    at the [start] address. (This includes writing
+    the start vector as the first two bytes of the
+    image)
+
+    Failures are indicated on the main LED array:
+
+    bit7[oooooooo]bit0
+
+    0) - no FRAM inserted, system stops in loop
+        leds will flash fast [ooooxxxx]<->[xxxxoooo]
+
+    1) - Checksum error, system stops in loop
+        leds will [calulated]<->[checksum from file] 
+
+    3) - Success, system will boot to vector in RAM
+        leds will flash [oooooooo] once
+        then start addr    [start addr low] -> [start addr high]
+        leds will flash [oooooooo] once
+        then vector addr   [vector addr low] -> [vector addr high]
+        leds will flash [oooooooo] once
+        (start address should match vector address)
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -29,6 +71,9 @@ for(i=0;i<8;i++) {
 }
 DEVICE_6522_WRITE_A(VIA_0, 0xff);
 
+DEBUG_PORT_ON;
+DEBUG_PB2_ON; //Time loading
+
 fm25640b_open();
 
 image_size=0;
@@ -40,6 +85,7 @@ start_addr_hi = fm25640b_read_byte(HEADER_START_OFFSET+1);
 
 if (image_size_lo == 0x0 && image_size_hi == 0x0 && start_addr_lo == 0x0 && start_addr_hi == 0x0) {
     //If FRAM isn't plugged in, system will read all 0x0
+    fm25640b_close();
     while(1) {
     DEVICE_6522_WRITE_A(VIA_0, 0xf0);
     for(ii=0;ii<5000;ii++);
@@ -51,15 +97,9 @@ if (image_size_lo == 0x0 && image_size_hi == 0x0 && start_addr_lo == 0x0 && star
 image_size = image_size_hi << 8;
 image_size |= image_size_lo;
 
-if (image_size > SBC_CODE_TOP) {
-    //sanity check
-}
-
-
 checksum=0;
 checksum += image_size_lo;
 checksum += image_size_hi;
-
 
 image_size+=DATA_OFFSET;
 for (ii=DATA_OFFSET;ii<image_size;ii++) {
@@ -69,7 +109,11 @@ for (ii=DATA_OFFSET;ii<image_size;ii++) {
     checksum += b;
 }
 
+
 b = fm25640b_read_byte(ii);
+fm25640b_close();
+
+DEBUG_PB2_OFF; //Time loading
 
 if (checksum != b) {
     //did't succeed flash...
@@ -102,8 +146,10 @@ for(ii=0;ii<20000;ii++);
 DEVICE_6522_WRITE_A(VIA_0, 0xff);
 for(ii=0;ii<40000;ii++);
 
-
-__asm__ ("jmp (%w)",0x200);
+// Load the address stored in 0x200 into the function pointer
+start_vector = (void (*)())(*(uint16_t*)0x0200);
+start_vector();
+//__asm__ ("jmp (%w)",0x200); //This, gets optimized out :(
 while(1);
 
 return EXIT_SUCCESS;
